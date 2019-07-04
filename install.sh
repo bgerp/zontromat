c524373624fbc33e42c1e4aaf3c7ca08c6cda00d
mitko <mitko@experta.bg>
2019-07-04 16:58:52 +0300

    WEB-a като сървис

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 sudo apt-get install -y php-cli php-sqlite
 
 crontab -l > cron.res
-echo "@reboot php -S 0.0.0.0:8181 -t ~/zontromat/webroot" > cron.res
+echo "@reboot php -S 0.0.0.0:8181 -t ~/zontromat/webroot 2>&1 &" > cron.res
 echo "* * * * * php ~/zontromat/cron.php" >> cron.res
 crontab cron.res
 rm cron.res
